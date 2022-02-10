bb4b8b305c0fda65442000dca95e851a80871fba
Abhishek Dewan <abhishekdewan2802@gmail.com>
2022-02-09 22:18:54 -0800

    SplashScreen: Some what of a good looking splash screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -5,6 +5,43 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("SplashScreen")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 75.0),
+              child: Text(
+                "Mind",
+                style: Theme.of(context)
+                    .textTheme
+                    .displaySmall
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Icon(
+              Icons.psychology_outlined,
+              size: 48,
+              color: Theme.of(context).textTheme.displaySmall?.color,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 75.0),
+              child: Text(
+                "Palace",
+                style: Theme.of(context)
+                    .textTheme
+                    .displaySmall
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: CircularProgressIndicator(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
